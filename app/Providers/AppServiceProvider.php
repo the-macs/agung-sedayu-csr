c0c4c75804e9fb3441cdb7a061bbb90d0f3f8258
ict-gdps <ict@garudapratama.com>
2025-12-11 21:04:11 +0700

    feat: Configure Livewire routes and asset base URL, and remove custom Filament asset path.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Filament\View\PanelsRenderHook;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentView;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        Livewire::setUpdateRoute(function ($handle) {
+            return '/csr-bedah-rumah/livewire/update';
+        });
+
+        Livewire::setScriptRoute(function () {
+            return '/csr-bedah-rumah/livewire/livewire.js';
+        });
+
+        Livewire::setAssetBaseUrl(url('/csr-bedah-rumah/livewire'));
 
         FilamentView::registerRenderHook(
             PanelsRenderHook::HEAD_END,
