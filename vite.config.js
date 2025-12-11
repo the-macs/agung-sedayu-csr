5cac88df6b56088f2176f0210df2ebc9e7ca724a
ict-gdps <ict@garudapratama.com>
2025-12-11 20:37:20 +0700

    fix: modify vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/filament/app/theme.css'
+                'resources/css/filament/admin/theme.css'
             ],
             refresh: true,
         }),
