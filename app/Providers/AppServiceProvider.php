fadb77a4d180e71110e67e40974d50638148fe22
ict-gdps <ict@garudapratama.com>
2025-12-11 21:44:56 +0700

    fix: fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
 
-        URL::forceRootUrl(config('app.url'));
-
         FilamentView::registerRenderHook(
             PanelsRenderHook::HEAD_END,
             fn(): string => '<style>
